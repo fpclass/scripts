d361e2dbecd7caeaa27ba351198d664ef06f5d76
Michael Gale <m.gale@warwick.ac.uk>
2018-12-11 12:19:40 +0000

    Add install.sh script

---
+++ install.sh
@@ -0,0 +1,6 @@
+# installs the scripts into /modules/cs141 folder
+cp haskell-setup.sh /modules/cs141
+cp update-paths.sh /modules/cs141
+
+# give haskell-setup.sh the correct permissions
+chmod o+rx /modules/cs141/haskell-setup.sh
