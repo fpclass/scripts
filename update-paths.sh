1e188bf2eb2566b7f5cc445fa2497e826e28b63d
Michael Gale <m.gale@warwick.ac.uk>
2018-12-11 12:22:49 +0000

    Update script for lts-12.14

--- update-paths.sh
+++ update-paths.sh
@@ -1,11 +1,14 @@
 # update absolute paths in package repository
 find /modules/cs141/.stack/snapshots/x86_64-linux/lts-9.0/8.0.2/pkgdb/ -type f -name "*.conf" | xargs sed -i 's/\/dcs\/acad\/u1672682/\/modules\/cs141/g'
+find /modules/cs141/.stack/snapshots/x86_64-linux/lts-12.14/8.4.3/pkgdb/ -type f -name "*.conf" | xargs sed -i 's/\/dcs\/acad\/u1672682/\/modules\/cs141/g'
 
 # remove the existing package database
 rm /modules/cs141/.stack/snapshots/x86_64-linux/lts-9.0/8.0.2/pkgdb/package.cache
+rm /modules/cs141/.stack/snapshots/x86_64-linux/lts-12.14/8.4.3/pkgdb/package.cache
 
 # rebuild the package database
 /modules/cs141/bin/ghc-pkg --package-db /modules/cs141/.stack/snapshots/x86_64-linux/lts-9.0/8.0.2/pkgdb/ recache
+/modules/cs141/bin/ghc-pkg --package-db /modules/cs141/.stack/snapshots/x86_64-linux/lts-12.14/8.4.3/pkgdb/ recache
 
 # update permissions so that everyone can access the files
 chmod -R o+rx /modules/cs141/.stack/
