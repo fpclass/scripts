187fbe9b9eabdb4ea4a91ab0960a9b460acb2eef
Michael Gale <m.gale@warwick.ac.uk>
2019-10-30 14:17:17 +0000

    Add support for lts-14.12

--- update-paths.sh
+++ update-paths.sh
@@ -1,14 +1,17 @@
 # update absolute paths in package repository
 find /modules/cs141/.stack/snapshots/x86_64-linux/lts-9.0/8.0.2/pkgdb/ -type f -name "*.conf" | xargs sed -i 's/\/dcs\/acad\/u1672682/\/modules\/cs141/g'
 find /modules/cs141/.stack/snapshots/x86_64-linux/lts-12.14/8.4.3/pkgdb/ -type f -name "*.conf" | xargs sed -i 's/\/dcs\/acad\/u1672682/\/modules\/cs141/g'
+find /modules/cs141/.stack/snapshots/x86_64-linux/lts-14.12/8.6.5/pkgdb/ -type f -name "*.conf" | xargs sed -i 's/\/dcs\/acad\/u1672682/\/modules\/cs141/g'
 
 # remove the existing package database
 rm /modules/cs141/.stack/snapshots/x86_64-linux/lts-9.0/8.0.2/pkgdb/package.cache
 rm /modules/cs141/.stack/snapshots/x86_64-linux/lts-12.14/8.4.3/pkgdb/package.cache
+rm /modules/cs141/.stack/snapshots/x86_64-linux/lts-14.12/8.6.5/pkgdb/package.cache
 
 # rebuild the package database
 stack exec --resolver=lts-9.0 -- ghc-pkg --package-db /modules/cs141/.stack/snapshots/x86_64-linux/lts-9.0/8.0.2/pkgdb/ recache
 stack exec --resolver=lts-12.14 -- ghc-pkg --package-db /modules/cs141/.stack/snapshots/x86_64-linux/lts-12.14/8.4.3/pkgdb/ recache
+stack exec --resolver=lts-14.12 -- ghc-pkg --package-db /modules/cs141/.stack/snapshots/x86_64-linux/lts-14.12/8.6.5/pkgdb/ recache
 
 # update permissions so that everyone can access the files
 chmod -R o+rx /modules/cs141/.stack/
