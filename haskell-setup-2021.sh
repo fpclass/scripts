12dbcf379fa57ba7ce153321b1fade319a28ab7b
Michael Gale <m.gale@warwick.ac.uk>
2020-12-22 15:04:02 +0000

    Update 20/21 setup script

--- haskell-setup-2021.sh
+++ haskell-setup-2021.sh
@@ -1,20 +1,15 @@
 # remove an existing .stack folder if there is one
 rm -f ~/.stack
 
-# create symbolic links
-mkdir ~/.stack
-mkdir ~/.stack/pantry
-ln -s /modules/cs141/2021/stack-data/pantry/pantry.sqlite3 ~/.stack/pantry/pantry.sqlite3
-ln -s /modules/cs141/2021/stack-data/config.yaml ~/.stack/config.yaml
-ln -s /modules/cs141/2021/stack-data/programs ~/.stack/programs
-ln -s /modules/cs141/2021/stack-data/setup-exe-cache ~/.stack/setup-exe-cache
-ln -s /modules/cs141/2021/stack-data/setup-exe-src ~/.stack/setup-exe-src
-ln -s /modules/cs141/2021/stack-data/snapshots ~/.stack/snapshots
-ln -s /modules/cs141/2021/stack-data/stack.sqlite3 ~/.stack/stack.sqlite3
+# Update .bashrc to add STACK_ROOT
+echo Updating .bashrc...
+echo 'export STACK_ROOT=/modules/cs141/2021/stack-data/' >> ~/.bashrc
 
 # install VSCode extensions
 echo Installing VSCode extensions...
-code --install-extension justusadam.language-haskell@3.3.0
-code --install-extension haskell.haskell@1.2.0
+code --log off --uninstall-extension justusadam.language-haskell
+code --log off --uninstall-extension haskell.haskell
+code --log off --install-extension justusadam.language-haskell@3.3.0
+code --log off --install-extension haskell.haskell@1.2.0
 
 echo Setup completed.
