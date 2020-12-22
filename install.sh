24d16c00ea93750c4804072ef2a2653bf1803193
Michael Gale <m.gale@warwick.ac.uk>
2020-12-22 14:54:57 +0000

    Fix install script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 cp haskell-setup-1920.sh /modules/cs141
 cp haskell-setup-2021.sh /modules/cs141
 rm -f /modules/cs141/haskell-setup.sh
-ln -s /modules/cs141/haskell-setup.sh /modules/cs141/haskell-setup-1920.sh
+ln -s /modules/cs141/haskell-setup-1920.sh /modules/cs141/haskell-setup.sh
 cp update-paths.sh /modules/cs141
 
 # give haskell-setup.sh the correct permissions
