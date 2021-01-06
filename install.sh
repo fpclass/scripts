fb671a16dd229163f8bdf66dabfed45239a919c8
Michael Gale <m.gale@warwick.ac.uk>
2021-01-06 16:20:50 +0000

    Remove old install code

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
 # installs the scripts into /modules/cs141 folder
-cp haskell-setup-1920.sh /modules/cs141
 cp haskell-setup-2021.sh /modules/cs141
 rm -f /modules/cs141/haskell-setup.sh
-ln -s /modules/cs141/haskell-setup-1920.sh /modules/cs141/haskell-setup.sh
-cp update-paths.sh /modules/cs141
+ln -s /modules/cs141/haskell-setup-2021.sh /modules/cs141/haskell-setup.sh
 
 # give haskell-setup.sh the correct permissions
-chmod o+rx /modules/cs141/haskell-setup-1920.sh
 chmod o+rx /modules/cs141/haskell-setup-2021.sh
 chmod o+rx /modules/cs141/haskell-setup.sh
